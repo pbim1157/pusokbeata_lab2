e3318372fa480ab76e5437b478203f9db48d4e5b
pusok beata <pusokbea@gmail.com>
2021-11-24 12:10:36 +0200

    laborator 3 am creat clasa doughnut

---
+++ DoughnutMachine.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Threading;
+
+namespace pusokbeata_lab2
+{
+
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+
+    }
+    class DoughnutMachine : Component
+    {
+        private System.Collections.ArrayList mDoughnuts = new System.Collections.ArrayList();
+
+        public delegate void DoughnutCompleteDelegate();
+        public event DoughnutCompleteDelegate DoughnutComplete; 
+
+
+        public DoughnutMachine()
+        {
+            InitializeComponent();
+        }
+
+        public DoughnutType Flavor { get; set; }
+
+
+        public Doughnut this[int Index]
+        {
+            get
+            {
+                return (Doughnut)mDoughnuts[Index];
+
+            }
+            set
+            {
+                mDoughnuts[Index] = value;
+            }
+
+        }
+
+
+        DispatcherTimer doughnutTimer;
+        private void InitializeComponent()
+        {
+            this.doughnutTimer = new DispatcherTimer();
+
+            this.doughnutTimer.Tick += new System.EventHandler(this.doughnutTimer_Tick);
+
+        }
+
+        private void doughnutTimer_Tick(object sender, EventArgs e)
+        {
+            Doughnut aDoughnut = new Doughnut(this.Flavor);
+            mDoughnuts.Add(aDoughnut);
+            DoughnutComplete();
+        }
+
+        public bool Enabled
+        {
+            set
+            {
+                doughnutTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                doughnutTimer.Interval = new TimeSpan(0, 0, value);
+            }
+        }
+
+        public void MakeDoughnuts(DoughnutType dFlavor)
+        {
+
+            Flavor = dFlavor;
+            switch (Flavor)
+            {
+                case DoughnutType.Glazed: Interval = 3; break;
+                case DoughnutType.Sugar: Interval = 2; break;
+                case DoughnutType.Lemon: Interval = 5; break;
+                case DoughnutType.Chocolate: Interval = 7; break;
+                case DoughnutType.Vanilla: Interval = 4; break;
+            }
+            doughnutTimer.Start();
+        }
+
+    }
+
+    
+    class Doughnut
+    {
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mTimeOfCreation = DateTime.Now;                                               // 
+            mFlavor = aFlavor;
+        }
+
+        private DoughnutType mFlavor;
+
+        public DoughnutType Flavor
+        {
+
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+
+        private float mPrice = .50F;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+
+        }
+       
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,98 @@ namespace pusokbeata_lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DoughnutMachine myDoughnutMachine;
+        private int m_RaisedGlazed;
+        private int m_RaisedSugar;
+        private int m_FilledLemon;
+        private int m_FilledChocolate;
+        private int m_FilledVanilla;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnButon1_Click(object sender, RoutedEventArgs e)
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
         {
-        //instructiuni
+            myDoughnutMachine = new DoughnutMachine();
+            myDoughnutMachine.DoughnutComplete += new
+            DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
+        }
+
+        private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = true;
+            sugarToolStripMenuItem.IsChecked = false;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+        private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = false;
+            sugarToolStripMenuItem.IsChecked = true;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
+        }
+
+        private void DoughnutCompleteHandler()
+        {
+            switch (myDoughnutMachine.Flavor)
+            {
+                case DoughnutType.Glazed:
+                    {
+                        m_RaisedGlazed++;
+                        txtGlazedRaised.Text = m_RaisedGlazed.ToString();
+                        break;
+                    }
+                case DoughnutType.Sugar:
+                    {
+                        m_RaisedSugar++;
+                        txtSugarRaised.Text = m_RaisedSugar.ToString();
+                        break;
+                    }
+
+                case DoughnutType.Lemon:
+                    {
+                        m_FilledLemon++;
+                        txtLemonFilled.Text = m_FilledLemon.ToString();
+                        break;
+                    }
+                case DoughnutType.Chocolate:
+                    {
+                        m_FilledChocolate++;
+                        txtChocolateFilled.Text = m_FilledChocolate.ToString();
+                        break;
+                    }
+                case DoughnutType.Vanilla:
+                    {
+                        m_FilledVanilla++;
+                        txtVanillaFilled.Text = m_FilledVanilla.ToString();
+                        break;
+                    }
+                default:
+                    {
+                        throw new InvalidEnumArgumentException($"Invalid Flavor {myDoughnutMachine.Flavor}");
+                    }
+            }
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine.Enabled = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void txtQuantity_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "Input Error", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+            }
         }
     }
 }
+
